c99ae0a7d3a9c8ed5fcf527f4db6d1b324f0f86e
Fengmian <fengmiansuperstar@gmail.com>
2020-03-26 22:27:16 -0400

    D

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+
+
+
+const { Job, Settings, CompanySettings, Schedules } = initSchema(schema);
+
+export {
+  Job,
+  Settings,
+  CompanySettings,
+  Schedules
+};
